63a46bb2d482130e5fd50ad4603d37182b29b906
michael li <michaelli@michaels-MacBook-Pro.local>
2018-03-02 21:35:33 +0800

    first commit

---
+++ ide_profiles/xcode/ExtendedKF.build/Debug/ZERO_CHECK.build/Script-3D4A5B1922B24F7C9AB85AA9.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/michaelli/Documents/sdc/t2/p1/ide_profiles/xcode -f /Users/michaelli/Documents/sdc/t2/p1/ide_profiles/xcode/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
